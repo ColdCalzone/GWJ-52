8da35d562667e8fe25ef610af75de73a11f1f11a
ColdCalzone <coldhotpocket1@gmail.com>
2022-12-11 04:21:23 -0500

    uh oh i forgot about committing for 9 hours

---
+++ game/objects/Grid.gd
@@ -0,0 +1,55 @@
+tool
+extends Node2D
+
+export var size = Vector2(1, 1)
+var rect_scale = Vector2(20, 14)
+
+var half_size = size * rect_scale / 2
+
+var mouse_offset = rect_scale/2
+
+const BACK_COLOR : Color = Color("141013")
+const GRID_COLOR : Color = Color("221c1a")
+
+var time = 0
+
+var grid : Dictionary = {}
+
+func _ready():
+	for block in get_tree().get_nodes_in_group("mirror_blocks"):
+		block.connect("placed", self, "snap_block_to_position")
+	for entity in get_tree().get_nodes_in_group("grid_object"):
+		snap_to_position(entity, entity.position)
+	half_size = size * rect_scale / 2
+
+func _draw():
+	var area = Rect2(-half_size, size * rect_scale)
+	area.size.x += 1
+	area.size.y += 1
+	draw_rect(area, BACK_COLOR)
+	for x in range(0, area.size.x, rect_scale.x):
+		draw_line(Vector2(x + 0.5, 0) - half_size, Vector2(x + 0.5, area.size.y) - half_size, GRID_COLOR, 1.05)
+	for y in range(0, area.size.y, rect_scale.y):
+		draw_line(Vector2(0, y + 0.5) - half_size, Vector2(area.size.x, y + 0.5) - half_size, GRID_COLOR, 1.05)
+
+func _process(delta):
+	if Engine.editor_hint:
+		time += delta
+		if time > 1:
+			half_size = size * rect_scale / 2
+			update()
+			time = 0
+
+func snap_block_to_position(block : Node2D):
+	var mouse_pos = get_global_mouse_position() / scale
+	if -half_size < mouse_pos and mouse_pos < half_size:
+		snap_to_position(block, mouse_pos)
+
+func snap_to_position(entity : Node2D, pos : Vector2):
+	pos -= Vector2(rect_scale.x/(int(size.x) % 2 + 1), rect_scale.y/(int(size.y) % 2 + 1)) + mouse_offset
+	entity.position = Vector2(
+			stepify(pos.x, rect_scale.x),
+			stepify(pos.y, rect_scale.y)
+		) + Vector2(rect_scale.x/(int(size.x) % 2 + 1), rect_scale.y/(int(size.y) % 2 + 1)) + mouse_offset
+	var index = (entity.position + half_size) / rect_scale
+	grid[entity] = index
--- game/objects/MirrorBlock.gd
+++ game/objects/MirrorBlock.gd
@@ -1,3 +1,4 @@
+tool
 extends Sprite
 
 var being_dragged : bool = false
@@ -6,32 +7,44 @@ var delay = 0.150
 
 enum States {RIGHT, LEFT}
 
-var state = States.RIGHT
+export(States) var state = States.RIGHT
 
 var time_elapsed = 0.0
 
-var rotation_offset = 0
+var rotation_offset : int = 0
 
 export var ungrabbable = false
 
+onready var grid = get_tree().get_nodes_in_group("Grid")[0]
+
+signal placed(block)
+
 func _ready():
 	region_rect.position.y = 32 * int(ungrabbable)
+	$ClickArea.visible = true
 
 func _process(delta : float):
-	if being_dragged: 
-		position = get_global_mouse_position()
-	if rotation_offset != 0:
-		time_elapsed += delta
-	if time_elapsed >= delay:
-		time_elapsed = 0
-		region_rect.position.x = int(region_rect.position.x + (32 * clamp(rotation_offset, -1, 1))) % 128
-		if region_rect.position.x < 0:
-			region_rect.position.x = 96
-		rotation_offset -= clamp(rotation_offset, -1, 1)
+	if Engine.editor_hint:
+		region_rect.position.y = 32 * int(ungrabbable)
+		region_rect.position.x = state * 64
+	else:
+		if being_dragged: 
+			position = get_global_mouse_position() / grid.scale
+		if rotation_offset != 0:
+			time_elapsed += delta
+		if time_elapsed >= delay:
+			time_elapsed = 0
+			region_rect.position.x = int(region_rect.position.x + (32 * clamp(rotation_offset, -1, 1))) % 128
+			if region_rect.position.x < 0:
+				region_rect.position.x = 96
+			rotation_offset -= clamp(rotation_offset, -1, 1)
 
 func _on_ClickArea_input_event(viewport, event : InputEvent, shape_idx):
-	if event is InputEventMouseButton:# and not ungrabbable:
+	if event is InputEventMouseButton and not ungrabbable:
 		being_dragged = event.pressed
+		if !being_dragged:
+			emit_signal("placed", self)
+			rotation_offset = sign(rotation_offset) * (int(rotation_offset * sign(rotation_offset)) % 2)
 
 func _input(event):
 	if being_dragged:
